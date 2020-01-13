257cabc8ba330d26d538882c2213119fe1672dd9
Eduardo Zimelewicz <Eduardo.Zimelewicz@marlo.com.au>
2020-01-13 16:45:22 -0300

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        [Route("Customers")]
+        public IActionResult Customers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 0, Name = "John Smith" },
+                new Customer { Id = 1, Name = "Mary Williams" }
+            };
+
+            var viewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public IActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 0, Name = "John Smith" },
+                new Customer { Id = 1, Name = "Mary Williams" }
+            };
+
+            return View(customers.ElementAt(id));
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [Route("Movies")]
+        public IActionResult Movies()
+        {
+            var movies = new List<Movie> 
+            { 
+                new Movie { Name = "Shrek" },
+                new Movie { Name = "Wall-e" }
+            };
+
+            var viewModel = new MoviesViewModel
+            {
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+    }
+}
